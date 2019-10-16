06ba93d00bfd6411bd43bf7f5e86df1c3fa3053d
Zhivula <56290740+Zhivula@users.noreply.github.com>
2019-10-16 11:19:55 +0300

    Добавлен метод записи данных о доходах в базу данных.

--- ZhiMoney/Model/IncomeModel.cs
+++ ZhiMoney/Model/IncomeModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using ZhiMoney.DataBase;
 
 namespace ZhiMoney.Model
 {
     class IncomeModel
     {
+        /// <summary>
+        /// Добавление записи в базу данных, в таблицу Income 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="summa"></param>
+        public void AddRecord(string name, float summa)
+        {
+            using (var context = new MyDbContext())
+            {
+                var income = new Income()
+                {
+                    Name = name,
+                    Summa = summa,
+                    Date = DateTime.Now
+                };
+                context.Incomes.Add(income);
+                context.SaveChanges();
+            }
+            MessageBox.Show("Запись успешно внесена!");
+        }
     }
 }
