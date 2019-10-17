be05969f11658183388c9e1d3dddec999073596c
Zhivula <56290740+Zhivula@users.noreply.github.com>
2019-10-17 10:24:38 +0300

    Добавлена функция внесения данных о расходах в базу данных

--- ZhiMoney/Model/ExpenseModel.cs
+++ ZhiMoney/Model/ExpenseModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using ZhiMoney.DataBase;
 
 namespace ZhiMoney.Model
 {
     class ExpenseModel
     {
+        /// <summary>
+        /// Добавление записи в базу данных, в таблицу Expense 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="summa"></param>
+        public void AddRecord(string name, float summa)
+        {
+            using (var context = new MyDbContext())
+            {
+                var expense = new Expense()
+                {
+                    Name = name,
+                    Summa = summa,
+                    Date = DateTime.Now
+                };
+                context.Expenses.Add(expense);
+                context.SaveChanges();
+            }
+            MessageBox.Show("Запись успешно внесена!");
+        }
     }
 }
