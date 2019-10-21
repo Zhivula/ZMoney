1099824f255bbfb243b724f2b2d1e8b0461e89cd
Zhivula <56290740+Zhivula@users.noreply.github.com>
2019-10-21 23:57:21 +0300

    Добавлена возможность: внесение данных о доходах в базу данных через нажатие кнопки из IncomeView.

--- ZhiMoney/ViewModel/IncomeViewModel.cs
+++ ZhiMoney/ViewModel/IncomeViewModel.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using ZhiMoney.Model;
 
 namespace ZhiMoney.ViewModel
 {
-    class IncomeViewModel
+    class IncomeViewModel : IncomeModel, INotifyPropertyChanged
     {
+        private string name;
+        private float summa;
+
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public float Summa
+        {
+            get => summa;
+            set
+            {
+                summa = value;
+                OnPropertyChanged(nameof(Summa));
+            }
+        }
+        public new ICommand AddRecord => new DelegateCommand(
+                o =>
+                {
+                    base.AddRecord(Name, Summa);
+                },
+                c =>
+                {
+                    //здесь нужна нормальная проверка, а не true
+                    //!string.IsNullOrWhiteSpace(Name) && Summa != null
+                    return true;
+                });
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
